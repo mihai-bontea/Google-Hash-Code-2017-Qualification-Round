d1350946e6fa7e41da9b529a745ebb7b8eba0271
mihai-bontea <mihaibontea@protonmail.com>
2025-03-06 16:31:20 +0200

    validating video sizes

--- score.py
+++ score.py
@@ -3,6 +3,11 @@ out_prefix = "output_files/sol1/";
 input_files = ["me_at_the_zoo.in", "trending_today.in", "kittens.in.txt", "videos_worth_spreading.in"]
 total_score = 0
 
+def vids_fit_on_caches(cache_size, vids_per_cache, video_sizes):
+    return all(sum(video_sizes[video_id] for video_id in vids_per_cache[cache_id]) <= cache_size 
+               for cache_id in vids_per_cache)
+
+
 for filename in input_files:
     full_path_in = in_prefix + filename
     full_path_out = out_prefix + filename.split(".", 1)[0] + ".out"
@@ -26,6 +31,14 @@ for filename in input_files:
                     cache_to_latency = {cache_index: latency for cache_index, latency in (map(int, in_file.readline().split()) for _ in range(nr_connected_caches))}
                 
                     endpoints.append((datacenter_latency, cache_to_latency))
+                
+                requests = [(vid_idx, end_idx, nr_req) for vid_idx, end_idx, nr_req in (map(int, in_file.readline().split()) for _ in range(nr_requests))]
+                
+                if not vids_fit_on_caches(cache_size, vids_per_cache_solution, video_sizes):
+                    print(f"Invalid output for file '{full_path_out}', videos don't fit on cache!")
+                    continue
+
+
 
                     
 
